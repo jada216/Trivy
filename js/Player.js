92f8f45b14a5797c1af70f39d7289dd8d98c6c76
Washington <qtp228@dca9047644ab.cof.ds.capitalone.com>
2017-08-21 10:54:12 -0400

    added some initial files

---
+++ js/Player.js
@@ -0,0 +1,8 @@
+class Player {
+  constructor(name) {
+    this.name = name;
+    this.score = 0;
+    this.categoriesNeeded = [['Sports', 0], ['Geography', 0], ['TV', 0], ['Science', 0], ['History', 0], ['Celebs', 0]]
+  }
+
+}
