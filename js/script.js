25c4f4a6ee9d6e97aff845b246a8ebf7f68dfb11
Washington <qtp228@dca9047644ab.cof.ds.capitalone.com>
2017-08-21 16:05:31 -0400

    added better styling and input form

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
 $(function (){
+  var player = '';
   $('#question').hide();
-  //var player = new Player(name);
+  $('#spin').hide();
+  $('#player-form').submit(function(e){
+    e.preventDefault();
+    player = new Player($('#playerName').val());
+    console.log(player.getName());
+
+    startGame();
+  });
+
+  $('#spin').click(function(){
+    $(this).hide();
+    $('#question').show();
+  });
+
+  $('.progressbar').addClass('bg-secondary');
 
 
-  //$('h4').text(name);
+
+
+
 
 
 });
+
+function startGame() {
+  $('#start').hide();
+  $('#spin').show();
+  $('h1').css('marginTop', '10vh');
+}
