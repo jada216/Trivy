176977a903a9c7e9d6f66c3861bbc4cefcabcb00
Washington <qtp228@dca9047644ab.cof.ds.capitalone.com>
2017-08-23 16:28:33 -0400

    got mvp basically just need to deal with score

--- js/script.js
+++ js/script.js
@@ -19,20 +19,18 @@ $(function() {
   $('#play').click(function() {
     $('#get-question').show();
     $('#player-input').hide();
-    $('#header-info').append('<h3>Choose A Topic</h3>');
     $('h1').css('marginTop', '5vh');
     $('#board').show();
     $('#player-score').append(`Score: 0`);
   });
 
-  $('.pieces').on('click', function() {
-    $('.modal-header').text($(this).text());
-    var $currentPiece = $(this);
-    $currentPiece.css('backgroundColor', '#5C9EAD').css('color', '#fff');
-    Game.getQuestion($(this).text()).done(function(data) {
+  $('#get-question').click(function(){
+    var random = Math.floor(Math.random() * Game.categories.length);
+    Game.getQuestion(Game.categories[random][0]).done(function(data) {
       $('.modal-footer button').attr('disabled', 'disabled');
       $('#answers-list').empty();
       console.log(data);
+      $('.modal-title').text(data.results[0].category);
       $('#modal-question').text(data.results[0].question);
       Game.correct = data.results[0].correct_answer;
       Game.incorrect = data.results[0].incorrect_answers;
@@ -66,8 +64,64 @@ $(function() {
           }
 
           if ($('.progress-bar').attr('aria-valuenow') === '100') {
-            $('pieces').removeAttr('disabled');
+            $('#header-info').text('<h3>Choose A Topic</h3>');
+            $('.pieces').removeAttr('disabled');
+            $('#get-question').hide();
           }
+
+          console.log(Player.consecutiveAnswers);
+          $('.correct').addClass('bg-success');
+        });
+        $('#answers-list').append($answer);
+        if (item.toLowerCase() === Game.correct.toLowerCase()) {
+          $answer.addClass('correct');
+        }
+      });
+    });
+  });
+
+  $('.pieces').on('click', function() {
+    $('.modal-header').text($(this).text());
+    var $currentPiece = $(this);
+    $currentPiece.css('backgroundColor', '#5C9EAD').css('color', '#fff');
+    Game.getQuestion($(this).text()).done(function(data) {
+      $('.modal-footer button').attr('disabled', 'disabled');
+      $('#answers-list').empty();
+      console.log(data);
+      $('#modal-question').text(data.results[0].question);
+      Game.correct = data.results[0].correct_answer;
+      Game.incorrect = data.results[0].incorrect_answers;
+
+      var answers = Game.incorrect;
+      var random = Math.floor(Math.random() * (Game.incorrect.length + 1));
+      answers.splice(random, 0, Game.correct);
+
+      answers.forEach(function(item, i) {
+        var $answer = $('<div>').addClass('answers btn btn-secondary');
+        $answer.text(item);
+        $answer.on('click', function() {
+          if ($(this).hasClass('correct')) {
+            Player.score++;
+            setTimeout(function() {
+              $('#modal-question').text('Close this Pop Up to continue');
+              $('#answers-list').empty();
+              $('#get-question').show();
+              Player.consecutiveAnswers = 0;
+              $('.progress-bar').css('width','0%');
+              $currentPiece.addClass('done').removeClass('pieces');
+              $('.pieces').attr('disabled', 'disabled');
+              $('.modal-footer button').removeAttr('disabled');
+              $currentPiece.attr('disabled', 'disabled');
+            }, 1500);
+          } else {
+            $(this).addClass('bg-danger');
+            setTimeout(function() {
+              $('#modal-question').text('Close this Pop Up to continue');
+              $('#answers-list').empty();
+              $('.modal-footer button').removeAttr('disabled');
+            }, 1500);
+          }
+
 
           console.log(Player.consecutiveAnswers);
           $('.correct').addClass('bg-success');
